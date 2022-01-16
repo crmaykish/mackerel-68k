a0375d76214c7f89e2d68c18d5e2f37df9acd81e
Colin Maykish <crmaykish@gmail.com>
2022-01-15 21:56:00 -0500

    Looping alphabet on 65C51 ACIA

--- firmware/nop_loop.s
+++ firmware/nop_loop.s
@@ -1,7 +1,7 @@
-    .equ PORTB, 0x80000
-    .equ PORTA, 0x80001
-    .equ DDRB, 0x80002
-    .equ DDRA, 0x80003
+    .equ ACIA_DATA, 0x80000
+    .equ ACIA_STATUS, 0x80001
+    .equ ACIA_COMMAND, 0x80002
+    .equ ACIA_CONTROL, 0x80003
 
     .section .vectors, #alloc
 
@@ -11,13 +11,70 @@ resetpc:    .long _start
     .section .text
 
 _start:
-    move.b #0xFF, DDRB
-    move.b #0xFF, DDRA
-    move.b #0xAA, PORTB
+    move.b #0x00, ACIA_STATUS
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    move.b #0x0B, ACIA_COMMAND
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    move.b #0b00010000, ACIA_CONTROL
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
 
-    move.w #0, %d0
+_restart:
+    move.b #0x0A, ACIA_DATA
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    move.b #0x0D, ACIA_DATA
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    move.b #'A', %d0
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
 _loop:
-    move.b %d0, PORTA
+    move.b %d0, ACIA_DATA
     add #1, %d0
 
     move.w #0x0000, %d1
@@ -26,5 +83,8 @@ _delay:
     addi.w #1, %d1
     cmpi.w #0x1000, %d1
     bne.s _delay
+
+    cmpi.b #'Z'+1, %d0
+    beq.s _restart
 
     jmp _loop
--- fpga/mackerel.v
+++ fpga/mackerel.v
@@ -1,10 +1,14 @@
 module mackerel(
-    input CLK, RST, A19, AS,
-    output ROM_EN, ACIA_EN
+    input CLK, RST, A19, AS, DS,
+    output ROM_EN, ACIA_EN,
+    output reg CLK2
 );
+    assign ROM_EN = ~(~A19 & ~AS & ~DS);
 
-    assign ROM_EN = ~(~A19 & ~AS);
+    assign ACIA_EN = ~(A19 & ~AS & ~DS);
 
-    assign ACIA_EN = ~(A19 & ~AS);
+    always @(posedge CLK) begin
+        CLK2 <= ~CLK2;
+    end
 
 endmodule
