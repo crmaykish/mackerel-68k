3954d7d4fa232158684a7233d9203df5149f4b46
Colin Maykish <crmaykish@gmail.com>
2022-01-16 17:36:26 -0500

    Check for TX ready flag before sending UART data

--- firmware/nop_loop.s
+++ firmware/nop_loop.s
@@ -3,6 +3,9 @@
     .equ ACIA_COMMAND, 0x80002
     .equ ACIA_CONTROL, 0x80003
 
+    .equ ACIA_TX_READY, 0x10
+    .equ ACIA_RX_READY, 0x08
+
     .section .vectors, #alloc
 
 resetsp:    .long 0xF0000
@@ -11,6 +14,16 @@ resetpc:    .long _start
     .section .text
 
 _start:
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    | Reset the ACIA
     move.b #0x00, ACIA_STATUS
     nop
     nop
@@ -21,6 +34,7 @@ _start:
     nop
     nop
     nop
+    | Send the ACIA init command
     move.b #0x0B, ACIA_COMMAND
     nop
     nop
@@ -31,18 +45,19 @@ _start:
     nop
     nop
     nop
+    | Set baudrate to 1/16 oscillator, no parity, 1 stop bit
     move.b #0b00010000, ACIA_CONTROL
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
 
 _restart:
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
+    nop
     move.b #0x0A, ACIA_DATA
     nop
     nop
@@ -63,28 +78,25 @@ _restart:
     nop
     nop
     nop
+
     move.b #'A', %d0
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
+
 _loop:
+    | Wait for TX ready status
+    move.b ACIA_STATUS, %d2
+    andi.b #ACIA_TX_READY, %d2 
+    cmpi.b #0x00, %d2
+    beq.s _loop
+
+    | Write byte to ACIA
     move.b %d0, ACIA_DATA
+
+    | Increment output byte
     add #1, %d0
 
-    move.w #0x0000, %d1
-
-_delay:
-    addi.w #1, %d1
-    cmpi.w #0x1000, %d1
-    bne.s _delay
-
+    | If byte is out of range, start over
     cmpi.b #'Z'+1, %d0
     beq.s _restart
 
+    | Send next byte
     jmp _loop
