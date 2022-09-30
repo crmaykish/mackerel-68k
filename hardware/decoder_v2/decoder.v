2ad39075cb3878b5776f85b3cda149fc9bd66c0e
Colin Maykish <crmaykish@gmail.com>
2022-09-29 22:07:37 -0400

    Cleanup decoder Verilog

--- hardware/decoder_v2/decoder.v
+++ hardware/decoder_v2/decoder.v
@@ -1,10 +1,10 @@
-module decoder(
+module mack_decoder_v2(
 	input CLK,
 	input RST,
 	input AS,
 	input DTACK_IN,
 	input IACK,
-	input [23:16] ADDR,
+	input [23:17] ADDR,
 	output ROMEN,
 	output RAMEN,
 	output DUARTEN,
@@ -44,11 +44,7 @@ module decoder(
 	assign DUARTEN = ~(IACK & ~AS & BOOT & ~ADDR[23] & ~ADDR[22] & ADDR[21] & ADDR[20] & ADDR[19] & ADDR[18] & ADDR[17]);
 
 	// DTACK
-	// TODO: Implement proper DTACK handling for DUART and interrupt handling
 	assign DTACK = 0;
-
-	// I don't think this is right
-	// assign DTACK = ~((~DUARTEN * ~DTACK_IN * IACK) | DUARTEN);
 
 
 endmodule
