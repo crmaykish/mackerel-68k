dc593fea79d209aac154aa4aebd11db298287d0e
Colin Maykish <crmaykish@gmail.com>
2022-11-16 20:32:57 -0500

    Double DUART board with independent interrupt channels

--- firmware/kernel.c
+++ firmware/kernel.c
@@ -3,19 +3,12 @@
 #include "mackerel.h"
 
 #define DUART_BASE_REGISTER 0x40
+#define DUART1_BASE_REGISTER 0x50
 
 void __attribute__((interrupt)) duart_isr()
 {
     // Determine the type of interrupt
     uint8_t misr = MEM(DUART_MISR);
-
-    if (misr & DUART_INTR_COUNTER)
-    {
-        // Counter timed out
-        MEM(DUART_OPR_RESET); // Read the OPR port to clear the counter and reset the timer. This clears the interrupt
-
-        mputc('X'); // Print an X when the timer ticks
-    }
 
     if (misr & DUART_INTR_RXRDY)
     {
@@ -25,22 +18,40 @@ void __attribute__((interrupt)) duart_isr()
     }
 }
 
+void __attribute__((interrupt)) duart1_isr()
+{
+    // Determine the type of interrupt
+    uint8_t misr = MEM(DUART1_MISR);
+
+    if (misr & DUART_INTR_COUNTER)
+    {
+        // Counter timed out
+        MEM(DUART1_OPR_RESET); // Read the OPR port to clear the counter and reset the timer. This clears the interrupt
+
+        mputc('Y'); // Print a Y when the timer ticks
+    }
+}
+
 int main()
 {
     uint32_t i = 0;
 
     // Map an exception handler for the periodic timer interrupt
     set_exception_handler(DUART_BASE_REGISTER, &duart_isr);
+    set_exception_handler(DUART1_BASE_REGISTER, &duart1_isr);
 
-    // NOTE: Interrupts are not working with classic DUART
+    // Setup DUART0
+    MEM(DUART_IVR) = DUART_BASE_REGISTER; // Set interrupt base register
+    MEM(DUART_IMR) = DUART_INTR_RXRDY;    // Unmask interrupts
+    MEM(DUART_OPR);                       // Start counter
 
-    // Setup DUART timer as 50 Hz interrupt
-    MEM(DUART_IVR) = DUART_BASE_REGISTER;                     // Set interrupt base register
-    MEM(DUART_ACR) = 0xF0;                                    // Set timer mode X/16
-    MEM(DUART_CUR) = 0x09;                                    // Counter upper byte, (3.6864MHz / 2 / 16 / 0x900) = 50 Hz
-    MEM(DUART_CLR) = 0x00;                                    // Counter lower byte
-    MEM(DUART_IMR) = (DUART_INTR_COUNTER | DUART_INTR_RXRDY); // Unmask interrupts
-    MEM(DUART_OPR);                                           // Start counter
+    // Setup DUART1
+    MEM(DUART1_IVR) = DUART1_BASE_REGISTER; // Set interrupt base register
+    MEM(DUART1_ACR) = 0xF0;                 // Set timer mode X/16
+    MEM(DUART1_CUR) = 0x80;                 // Counter upper byte, (3.6864MHz / 2 / 16 / 0x900) = 50 Hz
+    MEM(DUART1_CLR) = 0x00;                 // Counter lower byte
+    MEM(DUART1_IMR) = DUART_INTR_COUNTER;   // Unmask interrupts
+    MEM(DUART1_OPR);
 
     printf("Starting kernel...%X\r\n", 0xC0FFEE);
 
@@ -53,7 +64,7 @@ int main()
 
         i++;
 
-        delay(1000);
+        delay(100000);
     }
 
     return 0;
