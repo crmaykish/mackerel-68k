550f325f70f7277fa79580a1eee0ca4573449239
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-07-13 19:32:26 -0400

    Stripped down bootloader to fit in ROM alongside uClinux

--- firmware/bootloader.c
+++ firmware/bootloader.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 #include <string.h>
 #include "mackerel.h"
-#include "ch376s.h"
 
 #define INPUT_BUFFER_SIZE 32
 
@@ -11,11 +10,9 @@ void handler_runram();
 void handler_runrom();
 void handler_load(uint32_t addr);
 void handler_boot();
-void zero(uint32_t start, uint32_t end);
 uint8_t readline(char *buffer);
 void command_not_found(char *command);
 void memdump(uint32_t address, uint32_t bytes);
-uint32_t stack_pointer();
 void print_string_bin(char *str, uint8_t max);
 
 char buffer[INPUT_BUFFER_SIZE];
@@ -38,6 +35,10 @@ int main()
             uint32_t addr = strtoul(param1, 0, 16);
             handler_load(addr);
         }
+        else if (strncmp(buffer, "boot", 4) == 0)
+        {
+            handler_boot();
+        }
         else if (strncmp(buffer, "runrom", 6) == 0)
         {
             handler_runrom();
@@ -46,20 +47,6 @@ int main()
         {
             handler_runram();
         }
-        else if (strncmp(buffer, "boot", 4) == 0)
-        {
-            handler_boot();
-        }
-        else if (strncmp(buffer, "zero", 4) == 0)
-        {
-            strtok(buffer, " ");
-            char *param1 = strtok(NULL, " ");
-            char *param2 = strtok(NULL, " ");
-            uint32_t start = strtoul(param1, 0, 16);
-            uint32_t end = strtoul(param2, 0, 16);
-
-            zero(start, end);
-        }
         else if (strncmp(buffer, "dump", 4) == 0)
         {
             strtok(buffer, " ");
@@ -67,13 +54,6 @@ int main()
             uint32_t addr = strtoul(param1, 0, 16);
 
             memdump(addr, 256);
-        }
-        else if (strncmp(buffer, "stack", 5) == 0)
-        {
-            uint32_t sp = stack_pointer();
-
-            printf("Stack pointer: %X\r\n", sp);
-            memdump(sp, MEM_UINT(INIT_SP_ADDRESS) - sp);
         }
         else if (strncmp(buffer, "peek", 4) == 0)
         {
@@ -92,54 +72,6 @@ int main()
             uint8_t val = (uint8_t)strtoul(param2, 0, 16);
 
             MEM(addr) = val;
-        }
-        else if (strncmp(buffer, "memtest", 7) == 0)
-        {
-            strtok(buffer, " ");
-            char *param1 = strtok(NULL, " ");
-            char *param2 = strtok(NULL, " ");
-            uint32_t start = strtoul(param1, 0, 16);
-            uint32_t end = strtoul(param2, 0, 16);
-
-            int mem_errors = 0;
-
-            printf("Start first pass\r\n");
-
-            memset((void *)start, 0xAA, end - start);
-
-            for (int i = 0; i < end - start; i++)
-            {
-                if (i > 0 && (i % 0x1000) == 0)
-                {
-                    printf("%X\r\n", start + i);
-                }
-
-                if (MEM(start + i) != 0xAA)
-                {
-                    printf("Mem error at: %X: %X\r\n", start + i, MEM(start + i));
-                    mem_errors++;
-                }
-            }
-
-            printf("Start second pass\r\n");
-
-            memset((void *)start, 0x55, end - start);
-
-            for (int i = 0; i < end - start; i++)
-            {
-                if (i > 0 && (i % 0x1000) == 0)
-                {
-                    printf("%X\r\n", start + i);
-                }
-
-                if (MEM(start + i) != 0x55)
-                {
-                    printf("Mem error at: %X: %X\r\n", start + i, MEM(start + i));
-                    mem_errors++;
-                }
-            }
-
-            printf("Done! Total errors: %d", mem_errors);
         }
         else
         {
@@ -162,6 +94,12 @@ void handler_runrom()
 {
     mputs("Jumping to 0x200000\r\n");
     asm("jsr 0x200000");
+}
+
+void handler_boot()
+{
+    mputs("Jumping to 0x302000\r\n");
+    asm("jsr 0x302000");
 }
 
 void handler_load(uint32_t addr)
@@ -200,47 +138,6 @@ void handler_load(uint32_t addr)
     mputs("Done!");
 }
 
-// Load a kernel image into RAM, then jump to 0x8000
-void handler_boot()
-{
-    mputs("Copying IMAGE.BIN from USB drive...\r\n");
-
-    if (usb_reset() != CH376S_CMD_RET_SUCCESS)
-    {
-        mputs("Failed to reset USB module\r\n");
-        return;
-    }
-
-    size_t kernel_size = file_read("IMAGE.BIN", (uint8_t *)0x8000);
-
-    mputs("\r\n");
-
-    if (kernel_size == 0)
-    {
-        mputs("Failed to load image file.\r\n");
-        return;
-    }
-
-    mputs("\r\nKernel image loaded\r\n");
-
-    mputs("Jumping to 0x8000\r\n");
-
-    asm("jsr 0x8000");
-}
-
-void zero(uint32_t start, uint32_t end)
-{
-    if (end <= start)
-    {
-        printf("Error: end value must be higher than start value");
-        return;
-    }
-
-    printf("Zeroing RAM from %X to %X... ", start, end);
-    memset((void *)start, 0, end - start);
-    mputs("Done!");
-}
-
 void command_not_found(char *command_name)
 {
     mputs("Command not found: ");
@@ -269,16 +166,6 @@ uint8_t readline(char *buffer)
     buffer[count] = 0;
 
     return count;
-}
-
-uint32_t stack_pointer()
-{
-    uint32_t *stack_pointer;
-
-    asm volatile("move.l %%sp, %0"
-                 : "=p"(stack_pointer));
-
-    return (uint32_t)stack_pointer;
 }
 
 void print_string_bin(char *str, uint8_t max)
