a661654e9a75920bc0698a4b7c2cc4ac67783538
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-07-13 23:38:16 -0400

    3MB of RAM, 512K ROM, 512K I/O

--- firmware/vectors.s
+++ firmware/vectors.s
@@ -2,5 +2,5 @@
 
     .section .vectors, #alloc
 
-    .long 0x200000              | Initial stack pointer
+    .long 0x300000              | Initial stack pointer
     .long _start                | Program counter value
