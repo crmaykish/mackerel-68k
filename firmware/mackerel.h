58ddbbdab17f9018403703d69255f81a645bad25
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-09-15 22:32:16 -0400

    Update memory map to include DRAM

--- firmware/mackerel.h
+++ firmware/mackerel.h
@@ -12,7 +12,7 @@ typedef unsigned int uint32_t;
 
 #define LEDS 0xF00001
 #define GPIO 0xF00003
-#define DUART1_BASE 0xC00000
+#define DUART1_BASE 0xF80000
 #else
 #define SYSTEM_NAME "Mackerel-08"
 
