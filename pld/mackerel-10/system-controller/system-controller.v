2049068887eb0388c8a9a88c2200db6f7fecfd23
crmaykish <2431854+crmaykish@users.noreply.github.com>
2024-09-16 22:42:11 -0400

    Update memory map to fit DRAM

--- pld/mackerel-10/system-controller/system-controller.v
+++ pld/mackerel-10/system-controller/system-controller.v
@@ -22,20 +22,20 @@ module system_controller(
 	
 	output ROM_LOWER, ROM_UPPER,
 	output RAM_LOWER, RAM_UPPER,
-	output DUART,
+	
 	output EXP,
+	input DTACK_EXP,
 	
+	output DUART,
 	input IRQ_DUART,
-	
 	input DTACK_DUART,
-	
 	output IACK_DUART,
 	
 	output reg [7:0] GPIO
-	
 );
 
-assign EXP = 1;
+// Reconstruct the full address bus
+wire [24:0] ADDR_FULL = {ADDR_H, 9'b0, ADDR_L, 1'b0};
 
 assign BERR = 1;
 assign VPA = 1;
@@ -48,7 +48,10 @@ wire IACK = ~(FC0 && FC1 && FC2);
 
 assign IACK_DUART = ~(~IACK && ~AS && ~ADDR_L[3] && ~ADDR_L[2] && ADDR_L[1]);
 
-assign DTACK = ~((~DTACK_DUART && (~DUART || ~IACK_DUART)) || DUART);
+// TODO: confirm this DTACK logic
+wire DTACK0 = ~DTACK_DUART && (~DUART || ~IACK_DUART);
+wire DTACK1 = ~DTACK_EXP && ~EXP;
+assign DTACK = ~(DTACK0 || DTACK1 || (DUART && EXP));
 
 // Generate BOOT signal for first four bus cycles after reset
 reg BOOT = 1'b0;
@@ -69,7 +72,7 @@ end
 
 // Generate CPU clock from source oscillator
 reg [2:0] clk_buf = 0;
-assign CLK_CPU = clk_buf[1];	// Divide source clock by 4 to get CPU clock
+assign CLK_CPU = clk_buf[0];	// Divide source clock by 2 to get CPU clock
 always @(posedge CLK) clk_buf <= clk_buf + 3'b1;
 
 // Handle memory addressable GPIO on CPLD
@@ -81,28 +84,31 @@ always @(posedge CLK_CPU) begin
 		end
 	else
 		// LED at 0xF00001
-		if (ADDR_H[23] && ADDR_H[22] && ADDR_H[21] && ADDR_H[20] && ~ADDR_L[1]) begin
+		if (ADDR_H[23] && ADDR_FULL == 24'hF00000) begin
 			if (~LDS && ~RW) LED <= DATA[2:0];
 		end
 		
 		// GPIO at 0xF00003
-		if (ADDR_H[23] && ADDR_H[22] && ADDR_H[21] && ADDR_H[20] && ADDR_L[1]) begin
+		if (ADDR_H[23] && ADDR_FULL == 24'hF00002) begin
 			if (~LDS && ~RW) GPIO <= DATA[7:0];
 		end
 end
 
-// ROM enabled at 0x800000 - 0x900000
-wire ROM_EN = ~BOOT || (IACK && ADDR_H[23] && ~ADDR_H[22] && ~ADDR_H[21] && ~ADDR_H[20]);
+// ROM enabled at 0xE00000 - 0xF00000
+wire ROM_EN = ~BOOT || (IACK && ADDR_FULL >= 24'hE00000 && ADDR_FULL < 24'hF00000);
 assign ROM_LOWER = ~(~AS && ~LDS && ROM_EN);
 assign ROM_UPPER = ~(~AS && ~UDS && ROM_EN);
 
 // SRAM enabled at 0x000000 - 0x100000 (except at boot)
-wire RAM_EN = BOOT && IACK && ~ADDR_H[23] && ~ADDR_H[22] && ~ADDR_H[21] && ~ADDR_H[20];
+wire RAM_EN = BOOT && IACK && ADDR_FULL < 24'h100000;
 assign RAM_LOWER = ~(~AS && ~LDS && RAM_EN);
 assign RAM_UPPER = ~(~AS && ~UDS && RAM_EN);
 
-
 // DUART_EN when addr is > 0xC00000 - 0xD00000
-assign DUART = ~(BOOT && IACK && ~LDS && ADDR_H[23] && ADDR_H[22] && ~ADDR_H[21] && ~ADDR_H[20]);
+assign DUART = ~(BOOT && IACK && ~LDS && ADDR_FULL >= 24'hC00000 && ADDR_FULL < 24'hD00000);
+
+// DRAM at 0x100000 - 0x900000
+wire DRAM_EN = BOOT && IACK && ADDR_FULL >= 24'h100000 && ADDR_FULL < 24'h900000;
+assign EXP = ~DRAM_EN;
 
 endmodule
