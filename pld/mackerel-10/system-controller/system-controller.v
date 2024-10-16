edc34c959d3810bca8ad7846f6fbc3569d1a6f87
crmaykish <2431854+crmaykish@users.noreply.github.com>
2024-10-15 23:02:11 -0400

    New pin mapping for glue CPLD

--- pld/mackerel-10/system-controller/system-controller.v
+++ pld/mackerel-10/system-controller/system-controller.v
@@ -3,7 +3,6 @@ module system_controller(
 	input RST,
 	
 	output CLK_CPU,
-	output reg [2:0] LED,
 	
 	output IPL0, IPL1, IPL2,
 	
@@ -12,7 +11,7 @@ module system_controller(
 	input [7:0] DATA,
 	
 	input [23:14] ADDR_H,
-	input [4:1] ADDR_L,
+	input [3:1] ADDR_L,
 	
 	input AS, UDS, LDS,
 	
@@ -21,29 +20,41 @@ module system_controller(
 	input FC0, FC1, FC2,
 	
 	output ROM_LOWER, ROM_UPPER,
-	output RAM_LOWER, RAM_UPPER,
+	output SRAM_LOWER, SRAM_UPPER,
 	
 	output EXP,
+	input IRQ_EXP,
 	input DTACK_EXP,
+	output IACK_EXP,
 	
 	output DUART,
 	input IRQ_DUART,
 	input DTACK_DUART,
 	output IACK_DUART,
 	
-	output [7:0] GPIO
+	output DRAM,
+	input DTACK_DRAM,
+	
+	input IDE_INT,
+	output IDE_CS,
+	input IDE_RDY,
+	output IDE_RD,
+	output IDE_WR,
+	output IDE_BUF,
+	
+	output reg [3:0] GPIO
 );
 
-assign GPIO[2] = ~(RW && ~AS && ~LDS);	//R
-assign GPIO[3] = ~(~RW && ~AS && ~LDS);		//W
+//assign GPIO[2] = ~(RW && ~AS && ~LDS);	//R
+//assign GPIO[3] = ~(~RW && ~AS && ~LDS);		//W
 
-assign GPIO[0] = ~(BOOT && ADDR_FULL >= 24'hA00000 && ADDR_FULL < 24'hA00F00);	// CSO
-assign GPIO[1] = 1'b1;	// CS1
+//assign GPIO[0] = ~(BOOT && ADDR_FULL >= 24'hA00000 && ADDR_FULL < 24'hA00F00);	// CSO
+//assign GPIO[1] = 1'b1;	// CS1
 
 //assign GPIO[4] = IDE_RDY; input
 
 // Reconstruct the full address bus
-wire [24:0] ADDR_FULL = {ADDR_H, 9'b0, ADDR_L, 1'b0};
+wire [24:0] ADDR_FULL = {ADDR_H, 10'b0, ADDR_L, 1'b0};
 
 assign BERR = 1;
 assign VPA = 1;
@@ -61,9 +72,16 @@ assign IACK_DUART = ~(~IACK && ~AS && ~ADDR_L[3] && ~ADDR_L[2] && ADDR_L[1]);
 //wire DTACK1 = ~DTACK_EXP && ~EXP;
 //assign DTACK = ~(DTACK0 || DTACK1 || (DUART && EXP));
 
-assign DTACK = (~EXP && DTACK_EXP);
+//assign DTACK = (~EXP && DTACK_EXP);
 
-//assign DTACK = 1'b0;
+assign DTACK = 1'b0;
+
+assign IACK_EXP = 1'b1;
+
+assign DRAM = 1'b1;
+
+assign EXP = 1'b1;
+
 
 // Generate BOOT signal for first four bus cycles after reset
 reg BOOT = 1'b0;
@@ -76,7 +94,7 @@ always @(posedge AS) begin
 	end
 	else begin
 		if (~BOOT) begin
-			bus_cycles <= bus_cycles + 4'b1;
+			bus_cycles <= bus_cycles + 3'b1;
 			if (bus_cycles == 4'd4) BOOT <= 1'b1;
 		end
 	end
@@ -84,28 +102,24 @@ end
 
 // Generate CPU clock from source oscillator
 reg [2:0] clk_buf = 0;
-assign CLK_CPU = clk_buf[0];	// Divide source clock by 2 to get CPU clock
-always @(posedge CLK) clk_buf <= clk_buf + 3'b1;
+assign CLK_CPU = clk_buf[2];	// Divide 40MHz source clock by 8 to get CPU clock
+always @(posedge CLK) clk_buf <= clk_buf + 1'b1;
 
 // Handle memory addressable GPIO on CPLD
+/*
 always @(posedge CLK_CPU) begin
 	if (~RST)
 		begin
-			LED <= 0;
-			//GPIO <= 0;
+			GPIO <= 0;
 		end
 	else
-		// LED at 0xF00001
-		if (ADDR_H[23] && ADDR_FULL == 24'hF00000) begin
-			if (~LDS && ~RW) LED <= DATA[2:0];
-		end
-/*
 		// GPIO at 0xF00003
-		if (ADDR_H[23] && ADDR_FULL == 24'hF00002) begin
-			if (~LDS && ~RW) GPIO <= DATA[7:0];
+		if (ADDR_FULL == 24'hF00002) begin
+			if (~LDS && ~RW) GPIO <= DATA[3:0];
 		end
-*/
+
 end
+*/
 
 // ROM enabled at 0xE00000 - 0xF00000
 wire ROM_EN = ~BOOT || (IACK && ADDR_FULL >= 24'hE00000 && ADDR_FULL < 24'hF00000);
@@ -114,14 +128,20 @@ assign ROM_UPPER = ~(~AS && ~UDS && ROM_EN);
 
 // SRAM enabled at 0x000000 - 0x100000 (except at boot)
 wire RAM_EN = BOOT && IACK && ADDR_FULL < 24'h100000;
-assign RAM_LOWER = ~(~AS && ~LDS && RAM_EN);
-assign RAM_UPPER = ~(~AS && ~UDS && RAM_EN);
+assign SRAM_LOWER = ~(~AS && ~LDS && RAM_EN);
+assign SRAM_UPPER = ~(~AS && ~UDS && RAM_EN);
 
 // DUART_EN when addr is > 0xC00000 - 0xD00000
 assign DUART = ~(BOOT && IACK && ~LDS && ADDR_FULL >= 24'hC00000 && ADDR_FULL < 24'hD00000);
 
+// IDE at 0xA00000
+assign IDE_CS = ~(BOOT && ADDR_FULL >= 24'hA00000 && ADDR_FULL < 24'hA00F00); ///LDS?
+assign IDE_BUF = ~(BOOT && ADDR_FULL >= 24'hA00000 && ADDR_FULL < 24'hA00F00);
+assign IDE_RD = ~(RW && ~AS && ~LDS);
+assign IDE_WR = ~(~RW && ~AS && ~LDS);
+
 // DRAM at 0x100000 - 0x900000
-wire DRAM_EN = BOOT && IACK && ADDR_FULL >= 24'h100000 && ADDR_FULL < 24'h900000;
-assign EXP = ~DRAM_EN;
+//wire DRAM_EN = BOOT && IACK && ADDR_FULL >= 24'h100000 && ADDR_FULL < 24'h900000;
+//assign EXP = ~DRAM_EN;
 
 endmodule
