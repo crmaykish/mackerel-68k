b9807c2ba78d1e5409cf47bc9cee95f30b09e614
crmaykish <2431854+crmaykish@users.noreply.github.com>
2024-10-17 16:21:24 -0400

    CPU at 10 MHz, DRAM at 20 MHz

--- pld/mackerel-10/dram_controller/dram_controller.v
+++ pld/mackerel-10/dram_controller/dram_controller.v
@@ -44,7 +44,7 @@ assign ADDR_OUT_11 = 1'b0;
 
 // Generate DRAM clock from source oscillator
 reg [1:0] clock_counter = 2'b0;
-wire CLK_DRAM = clock_counter[1];	// DRAM controller runs at 1/4 oscillator frequency
+wire CLK_DRAM = clock_counter[0];	// DRAM controller runs at 1/2 oscillator frequency
 always @(posedge CLK) begin
 	clock_counter <= clock_counter + 1'b1;
 end
