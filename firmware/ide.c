ca7308fe7328f839c8c3bc350ff76d23d8ee9aaa
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-10-17 22:28:06 -0400

    Implement LBA sector reading

--- firmware/ide.c
+++ firmware/ide.c
@@ -2,6 +2,15 @@
 #include "mackerel.h"
 #include <stdio.h>
 
+void read_sector(uint16_t *buf)
+{
+    for (uint16_t i = 0; i < 256; i++)
+    {
+        // IDE_wait_for_data_ready();   // needed at higher speeds?
+        buf[i] = MEM16(IDE_DATA);
+    }
+}
+
 void IDE_wait_for_device_ready()
 {
     while ((MEM(IDE_STATUS) & IDE_SR_DRDY) == 0)
@@ -16,13 +25,25 @@ void IDE_wait_for_data_ready()
     }
 }
 
-void IDE_read_sector(uint16_t *buf)
+void IDE_read_sector(uint16_t *buf, uint32_t lba)
 {
-    for (uint16_t i = 0; i < 256; i++)
-    {
-        // IDE_wait_for_data_ready();   // needed at higher speeds?
-        buf[i] = MEM16(IDE_DATA);
-    }
+    // NOTE: IDE sector count starts at 1
+
+    // NOTE: limited to 24 bit LBA addressing (~8GB)
+
+    // Select master drive
+    MEM(IDE_DRIVE_SEL) = 0xE0;
+
+    MEM(IDE_SECTOR_COUNT) = 1;
+
+    MEM(IDE_SECTOR_START) = (uint8_t)(lba & 0xFF);
+    MEM(IDE_LBA_MID) = (uint8_t)((lba >> 8) & 0xFF);
+    MEM(IDE_LBA_HIGH) = (uint8_t)((lba >> 16) & 0xFF);
+    
+    // Send the read sector command
+    MEM(IDE_COMMAND) = IDE_CMD_READ_SECTOR;
+    IDE_wait_for_data_ready();
+    read_sector(buf);
 }
 
 void IDE_device_info(uint16_t *buf)
@@ -30,7 +51,7 @@ void IDE_device_info(uint16_t *buf)
     printf("Reading IDE device info...\r\n");
     MEM(IDE_COMMAND) = IDE_CMD_IDENTIFY;
     IDE_wait_for_data_ready();
-    IDE_read_sector(buf);
+    read_sector(buf);
 
     // Model
     for (int i = 27; i <= 46; i++)
--- firmware/ide.h
+++ firmware/ide.h
@@ -7,9 +7,12 @@
 #define IDE_BASE 0xFFC000
 #define IDE_DATA IDE_BASE + 0x00
 // #define IDE_ERROR IDE_BASE + 0x02
-// #define IDE_FEATURE IDE_BASE + 0x02
+#define IDE_FEATURE IDE_BASE + 0x02
 #define IDE_SECTOR_COUNT IDE_BASE + 0x04
 #define IDE_SECTOR_START IDE_BASE + 0x06
+#define IDE_LBA_MID IDE_BASE + 0x08
+#define IDE_LBA_HIGH IDE_BASE + 0x0A
+#define IDE_DRIVE_SEL IDE_BASE + 0x0C
 #define IDE_STATUS IDE_BASE + 0x0E
 #define IDE_COMMAND IDE_BASE + 0x0E
 
@@ -18,6 +21,7 @@
 #define IDE_CMD_READ_SECTOR 0x20
 #define IDE_CMD_WRITE_SECTOR 0x30
 #define IDE_CMD_IDENTIFY 0xEC
+#define IDE_CMD_SET_FEATURES 0xEF
 
 // Status register bits
 #define IDE_SR_BSY 0x80  // Busy
@@ -33,7 +37,7 @@ void IDE_wait_for_device_ready();
 
 void IDE_wait_for_data_ready();
 
-void IDE_read_sector(uint16_t *buf);
+void IDE_read_sector(uint16_t *buf, uint32_t lba);
 
 void IDE_device_info(uint16_t *buf);
 
