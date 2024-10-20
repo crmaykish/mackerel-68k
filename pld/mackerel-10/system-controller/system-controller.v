6f34c82c2e4511829081f612b10bb041e593c493
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-10-20 14:58:56 -0400

    Cleaned up signal names in glue Verilog

--- pld/mackerel-10/system-controller/system-controller.v
+++ pld/mackerel-10/system-controller/system-controller.v
@@ -1,77 +1,75 @@
 module system_controller(
 	input CLK,
-	input RST,
+	input RST_n,
 	
-	// TODO: update all the names to include _n if active low
-
 	output CLK_CPU,
 	
-	output IPL0, IPL1, IPL2,
+	output IPL0_n, IPL1_n, IPL2_n,
 	
-	output BERR, DTACK,
+	output BERR_n, DTACK_n,
 	
-	output reg VPA,
+	output reg VPA_n,
 	
 	input [7:0] DATA,
 	
 	input [23:14] ADDR_H,
 	input [3:1] ADDR_L,
 	
-	input AS, UDS, LDS, RW,
+	input AS_n, UDS_n, LDS_n, RW,
 	
 	input FC0, FC1, FC2,
 	
-	output ROM_LOWER, ROM_UPPER,
-	output SRAM_LOWER, SRAM_UPPER,
+	output CS_ROM0_n, CS_ROM1_n,
+	output CS_SRAM0_n, CS_SRAM1_n,
 	
-	output EXP,
-	input IRQ_EXP,
-	input DTACK_EXP,
-	output IACK_EXP,
+	output CS_EXP_n,
+	input IRQ_EXP_n,
+	input DTACK_EXP_n,
+	output IACK_EXP_n,
 	
-	output DUART,
-	input IRQ_DUART,
-	input DTACK_DUART,
-	output IACK_DUART,
+	output CS_DUART_n,
+	input IRQ_DUART_n,
+	input DTACK_DUART_n,
+	output IACK_DUART_n,
 	
-	output DRAM,
-	input DTACK_DRAM,
+	output CS_DRAM_n,
+	input DTACK_DRAM_n,
 	
 	input IDE_INT,
-	output IDE_CS,
+	output CS_IDE0_n,
 	input IDE_RDY,
-	output IDE_RD,
-	output IDE_WR,
-	output IDE_BUF,
+	output IDE_RD_n,
+	output IDE_WR_n,
+	output IDE_BUF_n,
 	
 	output [3:0] GPIO
 );
 
 // Unused signals
-assign BERR = 1;
-//assign IACK_EXP = 1;
-//assign EXP = 1'b1;
+assign BERR_n = 1;
+assign IACK_EXP_n = 1;
+assign CS_EXP_n = 1'b1;
 assign GPIO[1:0] = 2'b0;
 
 // Reconstruct the full address bus
 wire [24:0] ADDR_FULL = {ADDR_H, 10'b0, ADDR_L, 1'b0};
 
 // CPU is responding to an interrupt request
-wire IACK = ~(FC0 && FC1 && FC2);
+wire IACK_n = ~(FC0 && FC1 && FC2);
 
-assign IACK_DUART = ~(~IACK && ~AS && ADDR_L[3:1] == 3'd5);
+assign IACK_DUART_n = ~(~IACK_n && ~AS_n && ADDR_L[3:1] == 3'd5);
 
 // DTACK from DUART
-wire DTACK0 = ((~DUART || ~IACK_DUART) && DTACK_DUART);
+wire DTACK0 = ((~CS_DUART_n || ~IACK_DUART_n) && DTACK_DUART_n);
 // DTACK from DRAM
-wire DTACK1 = (~DRAM && DTACK_DRAM);
+wire DTACK1 = (~CS_DRAM_n && DTACK_DRAM_n);
 // DTACK to CPU
-assign DTACK = DTACK0 || DTACK1 || ~VPA;	// NOTE: DTACK and VPA cannot be LOW at the same time
+assign DTACK_n = DTACK0 || DTACK1 || ~VPA_n;	// NOTE: DTACK and VPA cannot be LOW at the same time
 
 
 // BOOT signal generation
 wire BOOT;
-boot_signal bs1(RST, AS, BOOT);
+boot_signal bs1(RST_n, AS_n, BOOT);
 
 // Generate CPU clock from source oscillator
 clock_gen cg1(CLK, CLK_CPU);
@@ -82,18 +80,13 @@ irq_encoder ie1(
 	.irq2(0),
 	.irq3(IDE_INT),
 	.irq4(0),
-	.irq5(~IRQ_DUART),
+	.irq5(~IRQ_DUART_n),
 	.irq6(IRQ_TIMER),
 	.irq7(0),
-	.ipl0_n(IPL0),
-	.ipl1_n(IPL1),
-	.ipl2_n(IPL2)
+	.ipl0_n(IPL0_n),
+	.ipl1_n(IPL1_n),
+	.ipl2_n(IPL2_n)
 );
-
-
-// DEBUG
-assign EXP = IDE_INT;
-assign IACK_EXP = IRQ_TIMER;
 
 
 // Generate a periodic timer interrupt (50 Hz)
@@ -108,14 +101,12 @@ always @(posedge CLK_CPU) begin
 		timer_buf <= 18'b0;
 	end
 	
-	// TODO: does VPA go LOW as soon as the IRQ goes low?
-	
 	// autovector the non-DUART interrupts
-	if (~IACK && IACK_DUART && ~AS) begin
-		VPA <= 1'b0;
+	if (~IACK_n && IACK_DUART_n && ~AS_n) begin
+		VPA_n <= 1'b0;
 		IRQ_TIMER <= 0;
 	end
-	else VPA <= 1'b1;
+	else VPA_n <= 1'b1;
 end
 
 //================================//
@@ -123,30 +114,29 @@ end
 //================================//
 
 // ROM at 0xF00000 (0x000000 on BOOT)
-wire ROM_EN = ~BOOT || (IACK && ADDR_FULL >= 24'hF00000 && ADDR_FULL < 24'hFF4000);
-assign ROM_LOWER = ~(~AS && ~LDS && ROM_EN);
-assign ROM_UPPER = ~(~AS && ~UDS && ROM_EN);
+wire ROM_EN = ~BOOT || (IACK_n && ADDR_FULL >= 24'hF00000 && ADDR_FULL < 24'hFF4000);
+assign CS_ROM0_n = ~(~AS_n && ~LDS_n && ROM_EN);
+assign CS_ROM1_n = ~(~AS_n && ~UDS_n && ROM_EN);
 
 // SRAM enabled at 0x000000 - 0x100000 (except at boot)
 /*
 wire RAM_EN = BOOT && IACK && ADDR_FULL < 24'h100000;
-assign SRAM_LOWER = ~(~AS && ~LDS && RAM_EN);
-assign SRAM_UPPER = ~(~AS && ~UDS && RAM_EN);
+assign CS_SRAM0_n = ~(~AS_n && ~LDS_n && RAM_EN);
+assign CS_SRAM1_n = ~(~AS_n && ~UDS_n && RAM_EN);
 */
 
 // DUART at 0xFF8000
-assign DUART = ~(BOOT && IACK && ~LDS && ADDR_FULL >= 24'hFF8000 && ADDR_FULL < 24'hFFC000);
+assign CS_DUART_n = ~(BOOT && IACK_n && ~LDS_n && ADDR_FULL >= 24'hFF8000 && ADDR_FULL < 24'hFFC000);
 
 // IDE at 0xFF4000 and 0xFFC000
-assign IDE_CS = ~(BOOT && IACK && ADDR_FULL >= 24'hFFC000);
-assign GPIO[2] = ~(BOOT && IACK && ADDR_FULL >= 24'hFF4000 && ADDR_FULL < 24'hFF8000);	// IDE CS1 pin (bodge)
-assign IDE_BUF = ~(~IDE_CS || ~GPIO[2]);
-assign IDE_RD = ~(RW && ~AS && ~UDS);
-assign IDE_WR = ~(~RW && ~AS && ~UDS);
+assign CS_IDE0_n = ~(BOOT && IACK_n && ADDR_FULL >= 24'hFFC000);
+assign GPIO[2] = ~(BOOT && IACK_n && ADDR_FULL >= 24'hFF4000 && ADDR_FULL < 24'hFF8000);	// IDE CS1 pin (bodge)
+assign IDE_BUF_n = ~(~CS_IDE0_n || ~GPIO[2]);
+assign IDE_RD_n = ~(RW && ~AS_n && ~UDS_n);
+assign IDE_WR_n = ~(~RW && ~AS_n && ~UDS_n);
 assign GPIO[3] = ~RW;	// IDE buffer DIR pin (bodge)
 
 // DRAM at 0x000000 - 0xF00000
-wire DRAM_EN = BOOT && IACK && ADDR_FULL < 24'hF00000;
-assign DRAM = ~DRAM_EN;
+assign CS_DRAM_n = ~(BOOT && IACK_n && ADDR_FULL < 24'hF00000);
 
 endmodule
