5422cf869449361a5f2557641bb2b192f6ad97e8
Colin Maykish <2431854+crmaykish@users.noreply.github.com>
2024-11-23 22:15:22 -0500

    Mapping ROM to 0x0000 at BOOT

--- firmware/vectors_30.s
+++ firmware/vectors_30.s
@@ -2,5 +2,5 @@
 
     .section .vectors, #alloc
 
-    .long 0x80080000            | Initial stack pointer
+    .long 0x80000               | Initial stack pointer
     .long _start                | Program counter value
--- pld/mackerel-30/system_controller/system_controller.v
+++ pld/mackerel-30/system_controller/system_controller.v
@@ -29,7 +29,7 @@ module system_controller(
 );
 
 // Reconstruct the full address bus value from components
-wire [31:0] ADDR = {AH, 8'b0, AM, 12'b0, AL};
+//wire [31:0] ADDR = {AH, 8'b0, AM, 12'b0, AL};
 
 assign DSACK0_n = 1'b0;	// All memory cycles are 8-bit
 assign DSACK1_n = 1'b1;
@@ -38,17 +38,21 @@ assign AVEC_n = 1'b1;
 assign CIIN_n = 1'b1;
 assign STERM_n = 1'b1;
 
+wire BOOT;
+boot_signal bs(RST_n, AS_n, BOOT);
+
 assign IPL_n = 3'b111;
 
 assign IACK_DUART_n = 1'b1;
 
-// ROM at 0
-assign CS_ROM_n = ~(~AS_n && ~AH[31] && ~AH[30]);
+// ROM at 0x0000 for first two memory cycles, 0x80000000 after that
+wire ROM_EN = ~BOOT || (AH[31] && ~AH[30]);
+assign CS_ROM_n = ~(~AS_n && ~DS_n && ROM_EN);
 
-// RAM at 0x80000000
-assign CS_SRAM_n = ~(~AS_n && ~DS_n && AH[31] && ~AH[30]);
+// RAM at 0x0000 after first two memory cycles
+assign CS_SRAM_n = ~(BOOT && ~AS_n && ~DS_n && ~AH[31] && ~AH[30]);
 
 // DUART at 0xC0000000
-assign CS_DUART_n = ~(~AS_n && ~DS_n && AH[31] && AH[30]);
+assign CS_DUART_n = ~(BOOT && ~AS_n && ~DS_n && AH[31] && AH[30]);
 
 endmodule
