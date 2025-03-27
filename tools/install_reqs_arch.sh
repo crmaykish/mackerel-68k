3edd38fa49a328a21c58ff23c9a54831480249ea
crmaykish <2431854+crmaykish@users.noreply.github.com>
2025-03-27 15:11:50 -0400

    baselibc build is broken

---
+++ tools/install_reqs_arch.sh
@@ -0,0 +1,3 @@
+echo "Installing required packages for Mackerel-68k build tools"
+
+sudo pacman -S --noconfirm base-devel wget unzip git bc
---
+++ tools/install_reqs_deb.sh
@@ -0,0 +1,6 @@
+echo "Installing required packages for Mackerel-68k build tools"
+
+sudo apt-get update
+sudo apt-get install -y build-essential flex bison libgmp3-dev libmpc-dev libmpfr-dev texinfo wget libncurses5-dev bc genromfs unzip git
+
+echo "Done!"
