d9975d3570733978c6ad5b38eedeb45ddbd24029
crmaykish <2431854+crmaykish@users.noreply.github.com>
2025-03-27 19:45:11 -0400

    Renamed mackerel build tools

--- tools/build_cross_compiler.sh
+++ tools/build_cross_compiler.sh
@@ -3,6 +3,10 @@
 # Note: the cross-compiler built by this script will build the bootloader and other bare-metal programs.
 # It will not build Linux/uClinux.
 
+#!/bin/sh
+
+set -e
+
 export PREFIX="/home/$(whoami)/mackerel/cross"
 export TARGET=m68k-mackerel-elf
 export PATH="$PREFIX/bin:$PATH"
@@ -17,53 +21,54 @@ log_message() {
     echo "======================================="
 }
 
-# rm -rf binutils* gcc* Baselibc
+rm -rf binutils* gcc* Baselibc
 
-# # Download binutils
-# log_message "Downloading binutils version ${BINUTILS_VERSION}"
-# wget -q "http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.gz"
+# Download binutils
+log_message "Downloading binutils version ${BINUTILS_VERSION}"
+wget -q "http://ftp.gnu.org/gnu/binutils/binutils-${BINUTILS_VERSION}.tar.gz"
 
-# log_message "Building binutils ${BINUTILS_VERSION}"
+log_message "Building binutils ${BINUTILS_VERSION}"
 
-# tar xzf "binutils-${BINUTILS_VERSION}.tar.gz"
-# cd "binutils-${BINUTILS_VERSION}"
-# mkdir build
-# cd build
-# ../configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-# make -j $(nproc)
-# make install
+tar xzf "binutils-${BINUTILS_VERSION}.tar.gz"
+cd "binutils-${BINUTILS_VERSION}"
+mkdir build
+cd build
+../configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
+make -j $(nproc)
+make install
 
-# log_message "Installed binutils ${BINUTILS_VERSION} to $PREFIX"
+log_message "Installed binutils ${BINUTILS_VERSION} to $PREFIX"
 
-# cd ../../
+cd ../../
 
 
-# # Download and build gcc
-# log_message "Downloading gcc version ${GCC_VERSION}"
-# wget -q "http://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz"
+# Download and build gcc
+log_message "Downloading gcc version ${GCC_VERSION}"
+wget -q "http://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz"
 
-# log_message "Building gcc ${GCC_VERSION}"
-# tar xzf "gcc-${GCC_VERSION}.tar.gz"
-# cd "gcc-${GCC_VERSION}"
-# mkdir build
-# cd build
-# ../configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
-# make -j $(nproc) all-gcc
-# make -j $(nproc) all-target-libgcc
-# make install-gcc
-# make install-target-libgcc
+log_message "Building gcc ${GCC_VERSION}"
+tar xzf "gcc-${GCC_VERSION}.tar.gz"
+cd "gcc-${GCC_VERSION}"
+mkdir build
+cd build
+../configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
+make -j $(nproc) all-gcc
+make -j $(nproc) all-target-libgcc
+make install-gcc
+make install-target-libgcc
 
-# log_message "Installed gcc ${GCC_VERSION} to $PREFIX"
+log_message "Installed gcc ${GCC_VERSION} to $PREFIX"
 
-# cd ../../
+cd ../../
 
 log_message "Installing Baselibc"
 
 git clone https://github.com/crmaykish/Baselibc.git
 cd Baselibc
-make PLATFORM=m68k-mackerel-elf
-cp -r include/ "${PREFIX}/m68k-mackerel-elf/"
-cp libc.a "${PREFIX}/m68k-mackerel-elf/lib/"
+
+make PLATFORM=mackerel
+cp -r include/ "${PREFIX}/${TARGET}/"
+cp libc.a "${PREFIX}/${TARGET}/lib/"
 
 log_message "Installed Baselibc"
 
@@ -73,4 +78,6 @@ log_message "Cleaning up"
 
 rm -rf binutils* gcc* Baselibc
 
-log_message "Done! Add ${PREFIX}/bin to your PATH to use the m68k-mackerel-elf- tools."
+log_message "Done! Add ${PREFIX}/bin to your PATH to use the ${TARGET} tools."
+
+set +e
